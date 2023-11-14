ed8a4218fea714bba38540ee2c03b2c6557863e1
Daniel Tan <danieltan380@gmail.com>
2023-11-13 16:35:23 -0800

    Include 'store.save' after creating stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,5 +6,6 @@ puts "----------"
 store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+store.save
 
-puts Stores.count
+puts Store.count
