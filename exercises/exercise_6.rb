b300fe36c0a91d0675a5e09c6be5549df3ced651
Daniel Tan <danieltan380@gmail.com>
2023-11-13 18:38:19 -0800

    Create three employees who belong to store1. Create three employees who belong to store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Daniel", last_name: "Tan", hourly_rate: 80)
+@store1.employees.create(first_name: "Bob", last_name: "Ross", hourly_rate: 90)
+@store1.employees.create(first_name: "Takumi", last_name: "Fujiwara", hourly_rate: 100)
+
+@store2.employees.create(first_name: "Hanzo", last_name: "Hasashi", hourly_rate: 60)
+@store2.employees.create(first_name: "Peter", last_name: "Parker", hourly_rate: 40)
+@store2. employees.create(first_name: "Duke", last_name: "Allen", hourly_rate: 30) 
