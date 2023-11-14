990505f7192cb9cecfbef5e8e0fcb40803111753
Daniel Tan <danieltan380@gmail.com>
2023-11-13 18:49:20 -0800

    Add prompts for users to create stores. Display error messages to the user if there are any

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,25 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+puts "What is the annual revenue? It must be a number:"
+annual_rev = gets.chomp
+
+puts "Does it have mens apparel? True or false:"
+mens = gets.chomp
+
+puts "Does it have womens apparel? True or false:"
+womens = gets.chomp
+
+new_store = Store.create(name: store_name, annual_revenue: annual_rev, mens_apparel: mens, womens_apparel: womens)
+
+if new_store.errors.any?
+  puts "There are errors when trying to save the store:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+else
+  puts "Store was saved!"
+end
